8482e1d82bc9407283acd6d34ff05d4fa1bf2a11
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2025-11-22 11:44:23 -0300

    feat: Adicionar controle de estoque mínimo e máximo

--- src/main/java/com/estoque/estoque/backend/dto/ProdutoDTO.java
+++ src/main/java/com/estoque/estoque/backend/dto/ProdutoDTO.java
@@ -15,7 +15,9 @@ public class ProdutoDTO {
     private String nome;
     private BigDecimal preco;
     private String categoria;
-    private Integer quantidadeEstoque; 
+    private Integer quantidadeEstoque;
+    private Integer quantidadeMinima;
+    private Integer quantidadeMaxima;
 
     public static ProdutoDTO fromEntity(Produto produto) {
         ProdutoDTO dto = new ProdutoDTO();
@@ -24,6 +26,8 @@ public class ProdutoDTO {
         dto.setPreco(produto.getPrecoUnitario());
         dto.setCategoria(produto.getCategoria() != null ? produto.getCategoria().getNome() : "Sem Categoria");
         dto.setQuantidadeEstoque(produto.getQuantidadeEstoque());
+        dto.setQuantidadeMinima(produto.getQuantidadeMinima());
+        dto.setQuantidadeMaxima(produto.getQuantidadeMaxima());
         return dto;
     }
 }
